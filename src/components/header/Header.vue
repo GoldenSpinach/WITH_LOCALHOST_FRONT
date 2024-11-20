660960e7dc13fcf06953021cb5a7c2e25b394c1d
HAEROOL <gofn080776@gmail.com>
2024-11-20 11:38:57 +0900

    feat: 헤더 레이아웃 구현

---
+++ src/components/header/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header class="flex h-[100px]  items-center justify-between px-[40px]">
+        <div class="w-[100px] h-[50px] bg-sky-500 rounded">
+            <img class="w-[100px] h-[50px]" src="@/assets/images/logo.png" alt="">
+        </div>
+        <div class="relative">
+            <div class="flex gap-[15px] p-[10px] border border-zinc-300 rounded-full cursor-pointer"
+                @click="toggleDropdown">
+                <img src="@/assets/images/menu.svg" alt="메뉴">
+                <img src="@/assets/images/default_profile_small.svg" alt="">
+            </div>
+            <div class="w-[150px] border border-zinc-300 rounded-lg absolute end-0 mt-[5px] shadow-xl" v-if="isToggled">
+                <ul class="flex flex-col" v-if="isLogin">
+                    <RouterLink to="/mypage" class="hover:bg-slate-200 p-[10px] cursor-pointer">MyPage</RouterLink>
+                    <li class="hover:bg-slate-200 p-[10px] cursor-pointer">Sign Out</li>
+                </ul>
+                <ul v-else>
+                    <li class="hover:bg-slate-200 p-[10px] cursor-pointer">Sign in</li>
+                    <li class="hover:bg-slate-200 p-[10px] cursor-pointer">Join</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang='ts'>
+import { onMounted, onUnmounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+
+const isLogin = ref(true)
+const isToggled = ref(false)
+const toggleDropdown = () => {
+    isToggled.value = !isToggled.value;
+};
+// 문서 클릭 이벤트 핸들러
+const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+
+    // 드롭다운 영역 내에서 클릭 시 아무 동작 안함
+    if (
+        target.closest('.relative') !== null
+    ) {
+        return;
+    }
+
+    // 드롭다운 외부 클릭 시 드롭다운 닫기
+    isToggled.value = false;
+};
+
+// 컴포넌트 마운트 시 이벤트 리스너 추가
+onMounted(() => {
+    document.addEventListener('click', handleClickOutside);
+});
+
+// 컴포넌트 해제 시 이벤트 리스너 제거
+onUnmounted(() => {
+    document.removeEventListener('click', handleClickOutside);
+});
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>랜딩페이지</div>
+
+    <section>
+        <form>
+            <input type="text">
+            <input type="date">
+            <input type="date">
+            <div>
+                태그 검색창
+            </div>
+            <img src="@/assets/images/search.svg" alt="검색">
+        </form>
+        <div>
+            <span>현지인이<br>호스팅하는<br>잊지못할 여행</span>
+            <div>
+                <img src="@/assets/images/default.png" alt="미리보기">
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang='ts'>
